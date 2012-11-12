6d2efb4065368a956565a44b009ef0c8ac6206e8
yarbelk <james.rivettcarnac@gmail.com>
2012-11-12 17:13:27 +0800

    genric urls and templates added.  templates are empty

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -7,5 +8,9 @@ urlpatterns = patterns('',
     # Admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
+    url(r'^$', TemplateView.as_view(template_name="location.html"), name="location"),
+    url(r'^$', TemplateView.as_view(template_name="contact.html"), name="contact"),
 
 )
