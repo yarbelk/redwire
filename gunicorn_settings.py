636d506b05609da4166271b98029fefafd3d0539
yarbelk <james.rivettcarnac@gmail.com>
2012-11-18 12:13:03 +0000

    added a run script because upstart was pissing me off

--- gunicorn_settings.py
+++ gunicorn_settings.py
@@ -3,7 +3,6 @@ import os
 
 bind = "127.0.0.1:8080"
 timeout = 120
-pidfile = os.path.dirname(__file__) + "process.pid"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = "/var/log/gunicorn/redwire.log"
 errorlog = "/var/log/gunicorn/redwire-error.log"
